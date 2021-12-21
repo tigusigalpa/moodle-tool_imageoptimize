23d25c6fbb0d18adb49e288749d7a9e183103a66
Peter Mayer <admin@pemasoft.de>
2021-12-21 13:48:56 +0100

    Mbs 5730 adding missing index (#4)
    * MBS-5677: Adding Index to tool_imageoptimize_files table
    
    * MBS-5730: Adding missing index contenthash new
    
    * MBS-5730: Bump version

--- db/upgrade.php
+++ db/upgrade.php
@@ -32,7 +32,7 @@ defined('MOODLE_INTERNAL') || die();
  * @return bool
  */
 function xmldb_tool_imageoptimize_upgrade($oldversion) {
-    global $CFG;
+    global $CFG, $DB;
 
     require_once($CFG->dirroot . "/admin/tool/imageoptimize/db/upgradelib.php");
 
@@ -43,6 +43,24 @@ function xmldb_tool_imageoptimize_upgrade($oldversion) {
 
         // Imageoptimize savepoint reached.
         upgrade_plugin_savepoint(true, 2020060801, 'tool', 'imageoptimize');
+    }
+
+    if ($oldversion < 2021122100) {
+        $dbman = $DB->get_manager();
+        $table = new xmldb_table('tool_imageoptimize_files');
+        $index = new xmldb_index('contenthashold', 'XMLDB_INDEX_NOTUNIQUE', ['contenthashold']);
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+    }
+
+    if ($oldversion < 2021122100) {
+        $dbman = $DB->get_manager();
+        $table = new xmldb_table('tool_imageoptimize_files');
+        $index = new xmldb_index('contenthashnew', null, ['contenthashnew']);
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
     }
 
     return true;
